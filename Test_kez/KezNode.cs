95c8d243812f6a613471380cdaeabb6984aa6b60
lleynov2018@mail.ru <lleynov2018@mail.ru>
2022-06-24 23:18:38 +0300

    Test

---
+++ Test_kez/KezNode.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+namespace test_test
+{
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+    public interface IKezface
+    {
+        public void jopa()
+        {
+            Console.WriteLine();
+        }
+    }
+    public class /*interface хуй знает,как оно работает*/  Search 
+    {
+       
+        public (string, int ) CasualSearch(int[] array, int searchvalue)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == searchvalue)
+                    return ("Значение найдено : yes", i);
+
+            }
+            return ($"Значение найдено : no", -1);
+        }
+        public int BinarySearch(int[] array, int searchvalue)
+        {
+            int min = 0;
+            int max = array.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchvalue == array[mid])
+                {
+                    return mid;
+                }
+                else if (searchvalue < array[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+
+        }
+        public int InterpolationSearch(int[] a, int value)
+        {
+            int min = 0;
+            int max = a.Length - 1;
+            while (min <= max)
+            {
+                // Находим разделяющий элемент
+                int mid = min + (max - min) * (value - a[min]) / (a[max] - a[min]);
+                if (a[mid] == value)
+                    return mid;
+                else if (a[mid] < value)
+                    min = mid + 1;
+                else if (a[mid] > value)
+                    max = mid - 1;
+            }
+            return -1; // Элемент не найден
+        }
+
+
+    }
+    public class DoublyLinkedList<T> : IEnumerable<T>  // двусвязный список
+    {
+        DoublyNode<T> head; // головной/первый элемент
+        DoublyNode<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        // добавление элемента
+        public void Add(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            DoublyNode<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+
+        // удаление
+        public bool Remove(T data)
+        {
+            DoublyNode<T> current = head;
+
+            // поиск удаляемого узла
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if (current != null)
+            {
+                // если узел не последний
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    // если последний, переустанавливаем tail
+                    tail = current.Previous;
+                }
+
+                // если узел не первый
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    // если первый, переустанавливаем head
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> BackEnumerator()
+        {
+            DoublyNode<T> current = tail;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Previous;
+            }
+        }
+        
+    }
+}
